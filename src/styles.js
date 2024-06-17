526bbec9f43c279aeef1365f70ee0fc251634d3a
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-06-17 20:30:24 +0000

    Added styles.js

---
+++ src/styles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'eact-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
+
+export default styles;
